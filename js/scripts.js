bbd2641df54eec6d3cc77e48a716b9dcc950aad9
William Kulha <kulha.william@gmail.com>
2018-09-14 14:05:32 -0400

    Add javascript so that the correct affiliation is displayed based on the user's input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var userTotalScore = function(val1, val2, val3, val4, val5, val6) {
+  return (val1 + val2 + val3 + val4 + val5 + val6);
+}
 
 $(document).ready(function(){
   $('form#userInput').submit(function(event){
@@ -8,7 +11,18 @@ $(document).ready(function(){
     var fox = parseInt($('input:radio[name=foxRadios]:checked').val());
     var abortion = parseInt($('input:radio[name=abortionRadios]:checked').val());
     var obama = parseInt($('input:radio[name=obamaRadios]:checked').val());
-    debugger;
+    var finalScore = userTotalScore(government, america, taxes, fox, abortion, obama);
 
-  })
-})
+    if (finalScore >= 15) {
+      $('div.hider').slideUp();
+      $('div#dem').show();
+    } else if (finalScore <= 14 && finalScore >= 9) {
+      $('div.hider').slideUp();
+      $('div#center').show();
+    } else {
+      $('div.hider').slideUp();
+      $('div#repub').show();
+    }
+
+  });
+});
